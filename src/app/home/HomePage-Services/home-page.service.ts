e1d0bf8d1c648933a3a59a22b029221eb6b4ce1d
Harshita Gupta <harshitagupta3199@gmail.com>
2023-05-24 11:24:43 +0000

    changed backend endpoint

--- src/app/home/HomePage-Services/home-page.service.ts
+++ src/app/home/HomePage-Services/home-page.service.ts
@@ -20,7 +20,8 @@ export class HomePageService {
 
   //Getting the Products from backend API
   getProducts():Observable<IProduct[]>{
-    let tempVar = this.http.get<IProduct[]>('https://localhost:5001/api/home/getproducts')
+    //let tempVar = this.http.get<IProduct[]>('https://localhost:5001/api/home/getproducts')
+    let tempVar = this.http.get<IProduct[]>('https://quickcart-backend.azurewebsites.net/api/home/getproducts')
     console.log(tempVar)
     return tempVar
   }
